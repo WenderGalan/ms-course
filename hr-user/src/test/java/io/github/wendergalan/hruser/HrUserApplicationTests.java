67306baed2b42f29a6b7234ecd17b7c34994e78b
Wender Galan <wendergalan2014@hotmail.com>
2020-10-24 17:07:43 -0400

    Criando projeto hr-user

---
+++ hr-user/src/test/java/io/github/wendergalan/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wendergalan.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
