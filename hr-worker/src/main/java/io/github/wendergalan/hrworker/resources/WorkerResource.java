c1ce9bd1f9737cfa1b6ff61a04d9770987fb8eee
Wender Galan <wendergalan2014@hotmail.com>
2020-10-24 09:27:32 -0400

    Balanceamento de carga com Ribbon

--- hr-worker/src/main/java/io/github/wendergalan/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/io/github/wendergalan/hrworker/resources/WorkerResource.java
@@ -3,6 +3,8 @@ package io.github.wendergalan.hrworker.resources;
 import io.github.wendergalan.hrworker.entities.Worker;
 import io.github.wendergalan.hrworker.repositories.WorkerRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +17,10 @@ import java.util.Optional;
 @RestController
 @RequestMapping(value = "/workers")
 @RequiredArgsConstructor
+@Log4j2
 public class WorkerResource {
     private final WorkerRepository repository;
+    private final Environment env;
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
@@ -25,6 +29,7 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        log.info("PORT = " + env.getProperty("local.server.port"));
         Optional<Worker> worker = repository.findById(id);
         return ResponseEntity.ok(worker.orElse(null));
     }
