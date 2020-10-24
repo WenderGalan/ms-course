fe3e41405f3a74e05c7790b9d9a0c5b0675f2a7e
Wender Galan <wendergalan2014@hotmail.com>
2020-10-24 08:52:15 -0400

    RestTemplate

--- hr-payroll/src/main/java/io/github/wendergalan/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/io/github/wendergalan/hrpayroll/services/PaymentService.java
@@ -1,15 +1,32 @@
 package io.github.wendergalan.hrpayroll.services;
 
 import io.github.wendergalan.hrpayroll.entities.Payment;
+import io.github.wendergalan.hrpayroll.entities.Worker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class PaymentService {
+    private final RestTemplate restTemplate;
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
 
     public Payment getPayment(long workerId, int days) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", String.valueOf(workerId));
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+
         return Payment.builder()
-                .name("Bob")
-                .dailyIncome(200.0)
+                .name(worker.getName())
+                .dailyIncome(worker.getDailyIncome())
                 .days(days)
                 .build();
     }
