153f2d223d32dfc7c77f8b8bba41c7e8a594f938
Wender Galan <wendergalan2014@hotmail.com>
2020-10-26 18:26:36 -0400

    Configurando projeto hr-oauth

--- hr-oauth/src/main/java/io/github/wendergalan/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/io/github/wendergalan/hroauth/HrOauthApplication.java
@@ -2,8 +2,10 @@ package io.github.wendergalan.hroauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HrOauthApplication {
 
 	public static void main(String[] args) {
