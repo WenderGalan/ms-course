a83788ca3f0444b6f33d0da71be3459578aa9b2a
Wender Galan <wendergalan2014@hotmail.com>
2020-10-26 18:07:27 -0400

    Carga inicial da base de dados (seeding)

--- hr-user/src/main/java/io/github/wendergalan/hruser/HrUserApplication.java
+++ hr-user/src/main/java/io/github/wendergalan/hruser/HrUserApplication.java
@@ -1,15 +1,26 @@
 package io.github.wendergalan.hruser;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class HrUserApplication {
+@Slf4j
+@RequiredArgsConstructor
+public class HrUserApplication implements CommandLineRunner {
+	private final BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// log.info("BCRYPT = " + passwordEncoder.encode("123456"));
+	}
 }
