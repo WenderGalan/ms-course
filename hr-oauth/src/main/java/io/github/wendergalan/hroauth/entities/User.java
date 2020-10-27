ec6383c634bd1d869f3eb1dfd0c82e147e9fe8ef
Wender Galan <wendergalan2014@hotmail.com>
2020-10-26 21:34:44 -0400

    Login e token JWT

--- hr-oauth/src/main/java/io/github/wendergalan/hroauth/entities/User.java
+++ hr-oauth/src/main/java/io/github/wendergalan/hroauth/entities/User.java
@@ -4,16 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
-public class User implements Serializable {
+public class User implements Serializable, UserDetails {
     private static final long serialVersionUID = 1L;
 
     @EqualsAndHashCode.Include
@@ -22,4 +27,37 @@ public class User implements Serializable {
     private String email;
     private String password;
     private Set<Role> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles
+                .stream()
+                .map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- hr-oauth/src/main/java/io/github/wendergalan/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/io/github/wendergalan/hroauth/services/UserService.java
@@ -4,12 +4,15 @@ import io.github.wendergalan.hroauth.entities.User;
 import io.github.wendergalan.hroauth.feignclients.UserFeignClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserFeignClient userFeignClient;
 
     public User findByEmail(String email) {
@@ -19,6 +22,14 @@ public class UserService {
             throw new IllegalArgumentException("Email not found");
         }
         log.info("Email found: " + email);
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(username).getBody();
+        if (user == null)
+            throw new UsernameNotFoundException("Email not found");
         return user;
     }
 }
